ecd94003cd8731e358ba2807af446e2fa38e3d78
Sneha Bista <snehabista727@gmail.com>
2023-11-29 20:44:07 -0600

    sneha's changes

--- test.py
+++ test.py
@@ -15,6 +15,9 @@ imgCount = 0
 # Create the input folder
 os.mkdir('./static')
 
+spacePressed = False
+
+
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -25,16 +28,16 @@ while True:
     
     # Display the captured frame
     cv2.imshow('Webcam', frame)
-
+    
     # Run YOLOv5 every 1 second
-    if time.time() - last_time > 1:
+    if spacePressed and time.time() - last_time > 1:
         last_time = time.time()
         run(
-            weights='BestWeight.pt',  # model.pt path(s)
+            weights='best.pt',  # model.pt path(s)
             source="frame.jpg",  # file/dir/URL/glob, 0 for webcam
             save_txt=True,  # save results to *.txt
             project = 'output',
-            nosave=True,  # do not save images/videos
+            #nosave=True,  # do not save images/videos
         )
         # Read the output file
         # CHeck if output/exp/labels have a txt file called frame.txt
@@ -44,24 +47,35 @@ while True:
             # Read all the lines
             lines = file.readlines()
             # Print the lines
+            eyes_open = False
             for line in lines:
                 print(line)
-
+                
             #TODO: Do custom stuff with the lines to save the image
-
+                firstChar = line[0]
+                if firstChar == "2":
+                    eyes_closed = True
+                
             # Save the image
+            if eyes_closed:
+                cv2.imwrite(f'./static/frame{imgCount}.jpg', frame)
+                imgCount += 1
+                spacePressed = False
             
-            cv2.imwrite(f'./static/frame{imgCount}.jpg', frame)
-            imgCount += 1
 
             # Close the file
             file.close()
-
+        #spacePressed = False
         shutil.rmtree('./output', ignore_errors=True)
 
     # Check for the 'q' key to exit the loop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
         break
+    elif key == ord('a'):
+        spacePressed = True
+        print("space pressed")
+        
 
 # Release the VideoCapture object and close the window
 cap.release()
